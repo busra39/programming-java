834f7df7ebafe272b2ea351c0d9229880c0bd7a0
busra <busracan39@gmail.com>
2018-09-16 23:31:13 +0300

    reverse_bytes added' '

---
+++ src/ReverseBytes.java
@@ -0,0 +1,23 @@
+public class ReverseBytes {
+
+    public static void main(String... args) {
+        reverse_bytes(new byte[]{'s','k','r','o','w','t','e','N',' ','z','z','a','J'});
+    }
+
+    public static void reverse_bytes(byte[] byteArray) {
+        if(byteArray.length == 0)
+            return;
+
+        int first = 0;
+        int last = byteArray.length -1;
+        while(first <= last) {
+            byte tmp = byteArray[last];
+            byteArray[last] = byteArray[first];
+            byteArray[first] = tmp;
+            first++;
+            last--;
+        }
+        for(int i =0; i< byteArray.length; i++)
+            System.out.print((char)byteArray[i]+ " ");
+    }
+}
